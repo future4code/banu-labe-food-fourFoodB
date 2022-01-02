6c916fac31591a55c33bb88583f46a4c80ac5726
PatriciaRGSacramento <patriciargsacramento@outlook.com>
2022-01-02 17:40:18 -0300

    alterações na página de detalhes

--- src/pages/PageInicial.jsx
+++ src/pages/PageInicial.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const PageInicial = () => {
   return (
     <div>
-      <Link to='/'>Home</Link> <Link to='/cart'>Cart</Link>{' '}
+      <Link to='/restaurants'>Home</Link> <Link to='/cart'>Cart</Link>{' '}
       <Link to='/account'>Acount</Link> <Link to='/login'>Login</Link>
       <Link to='/signup'>Cadastro</Link> 
       <Link to='/signup/newaddress'>Endereço</Link>
